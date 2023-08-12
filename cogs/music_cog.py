86f9518a9bdf8f1dd9e0976dc50f38f7237a316a
Luan Carlo <luanveloz@gmail.com>
2023-08-12 17:28:19 +0300

    feat: exit channel when playlist ends

--- cogs/music_cog.py
+++ cogs/music_cog.py
@@ -51,6 +51,7 @@ class music_cog(commands.Cog):
             self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
         else:
             self.is_playing = False
+            await self.vc.disconnect()
 
 
     # infinite loop checking 
@@ -78,6 +79,7 @@ class music_cog(commands.Cog):
             self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
         else:
             self.is_playing = False
+            await self.vc.disconnect()
 
 
     @commands.command(name="play", help="Plays a selected song from youtube")
