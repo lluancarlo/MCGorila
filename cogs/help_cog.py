237c9d27f300f3746a646468734a296f221ed0ef
Luan Carlo <luanveloz@gmail.com>
2023-08-13 14:57:07 +0300

    feat: random thumbnail on help command

--- cogs/help_cog.py
+++ cogs/help_cog.py
@@ -1,4 +1,5 @@
 import json
+import random
 import discord
 from discord.ext import commands
 
@@ -11,7 +12,7 @@ class help_cog(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
         self.text_channel_list = []
-        self.help_message = discord.Embed(
+        self.help_embed = discord.Embed(
             type="rich",
             color=discord.Colour.green(),
             title="Comandos",
@@ -30,6 +31,12 @@ class help_cog(commands.Cog):
 Se não gostou faz o seguinte: ***ME PANHA*** !
 """
         )
+        self.thumbnail_list = [
+            "https://media.tenor.com/MGo2ZucU3SEAAAAd/mc-gorila.gif",
+            "https://i.makeagif.com/media/4-29-2017/5wYPGI.gif",
+            "https://j.gifs.com/mlXY8P.gif",
+            "https://media.tenor.com/Nx_gxPSDzcsAAAAd/mc-gorila-thank-you.gif"
+        ]
 
     
     #some debug info so that we know the bot has started    
@@ -42,4 +49,5 @@ Se não gostou faz o seguinte: ***ME PANHA*** !
 
     @commands.command(name="help", help="Displays all the available commands")
     async def help(self, ctx) -> None:
-        await ctx.send(embed=self.help_message)
+        self.help_embed.set_thumbnail(url=self.thumbnail_list[random.randint(0, len(self.thumbnail_list))])
+        await ctx.send(embed=self.help_embed)
